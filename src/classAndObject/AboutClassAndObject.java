27f1841c15b02f8638c7b33eb5e554dd602dc588
TejPrakash18 <tej22upa.dhyay@gmail.com>
2023-07-08 17:48:56 +0530

    upadate class and object file

--- src/classAndObject/AboutClassAndObject.java
+++ src/classAndObject/AboutClassAndObject.java
@@ -2,13 +2,21 @@ package classAndObject;
 
 public class AboutClassAndObject {
     public static void main(String[] args) {
+        // create an object and calling argument constructor
         Human obj = new Human(21, 48.7F, (float) 5.3F,"Tej");
+        // calling  no argument constructor
+        Human ob = new Human();
         obj.sleep();
         obj.eat();
         String name = obj.name;
         System.out.println(name);
         int age = obj.age;
         System.out.println(age);
+        float height = obj.height;
+        System.out.println(height);
+        float weight = obj.weight;
+        System.out.println(weight);
+
     }
 }
 
@@ -30,5 +38,9 @@ class Human{
         this.weight=weight;
         this.height=height;
         this.name=name;
+    }
+    // no argument contractor
+    Human(){
+        System.out.println("No argument constractor :) ");
     }
 }
