9e1ab81d188c4933ff4879661e190052526ebe83
TejPrakash18 <tej22upa.dhyay@gmail.com>
2023-07-09 22:49:30 +0530

    update file

--- src/polymorphism/aboutPolymorphism.java
+++ src/polymorphism/aboutPolymorphism.java
@@ -1,4 +1,29 @@
 package polymorphism;
 
 public class aboutPolymorphism {
+    public static void main(String[] args) {
+        //object of compile time polymorphism
+        CompileTimePolymorphism  obj = new CompileTimePolymorphism();
+        obj.show();
+        obj.show(9,1);
+
+        //object of run time polymorphism
+
+
+
+    }
+}
+class CompileTimePolymorphism{
+    public void show(){
+        System.out.println("Polymorphism , withOut overloaded");
+    }
+    public void show(int a, int b){
+        System.out.println("method overloaded");
+        System.out.println("Add two number using method overloaded "+ (a+b));
+
+    }
+}
+
+class RunTimePolymorphism{
+
 }
