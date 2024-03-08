694aa20f22e37bafc7401df3059c4b3d93aa4cbd
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-03-08 21:44:47 +0530

    update

--- src/Projects/CBSEPercentCalculator/CBSEPercentCalculate.java
+++ src/Projects/CBSEPercentCalculator/CBSEPercentCalculate.java
@@ -18,16 +18,19 @@ class Student{
         }
         return (double) totalMarks/marks.length;
     }
+
+    public int totalMarks(){
+        int totalMarks =0;
+        for(int i : marks){
+            totalMarks+=i;
+        }
+        return totalMarks;
+    }
     public void setMarks(int[] marks) {
         this.marks = marks;
-
     }
 
     public int[] getMarks() {
-//        int sum =0;
-//        for (int i : marks){
-//            sum+=i;
-//        }
         return marks;
     }
 
@@ -46,6 +49,7 @@ public class CBSEPercentCalculate {
         System.out.print("Enter student name: ");
         String name = scanner.nextLine();
 
+
         System.out.print("Enter number of subjects: ");
         int numSubjects = scanner.nextInt();
         int[] marks = new int[numSubjects];
@@ -56,12 +60,34 @@ public class CBSEPercentCalculate {
         }
         Student student = new Student(name, marks);
         double percentage = student.calculatePercentage();
-//        System.out.println("Total marks " + student.getMarks() );
 
+
+        //get Marks function;
+        int[] studentMarks = student.getMarks();
+        System.out.print("Marks obtained: ");
+        for (int i = 0; i < studentMarks.length; i++) {
+            System.out.print(studentMarks[i]);
+            System.out.print("/100 ");
+            if (i != studentMarks.length - 1) {
+                System.out.print(", ");
+            }
+        }
+
+
+        System.out.println();
+
+        System.out.println("Total marks  :) "+ student.totalMarks());
 
         System.out.println("Percentage for " + student.getName() + ": " + percentage + "%");
         double cgpa = percentage / 9.5;
         System.out.println("CGPA for "+ student.getName()+ ": "+ cgpa);
+
+
+        //set name
+        String newName = scanner.nextLine();
+        student.setName("Bhumika");
+        System.out.println("Name of student is :" +student.getName());
+
 
         scanner.close();
     }
