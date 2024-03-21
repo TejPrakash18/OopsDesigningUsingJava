a9cb3ea249d02554e793c7a434400176c465bc5d
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-03-21 20:51:56 +0530

    FinalKeyWord

---
+++ src/FinalKeyWord/FinalKeyword.java
@@ -0,0 +1,26 @@
+package FinalKeyWord;
+
+final class Person{
+
+}
+
+//class Child extends Person{
+//
+//}
+
+class PersonOne{
+    final void FinalMethod(){
+        System.out.println("Hey");
+    }
+}
+//class ChildOne extends PersonOne{
+//    void FinalMethod(){
+//        System.out.println("kuchh nahin bhai");
+//    }
+//}
+public class FinalKeyword {
+    public static void main(String[] args) {
+        final int maxSize = 100;
+//        maxSize = 200;
+    }
+}
